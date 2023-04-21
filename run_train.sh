c9dbb6e138b1c9e2f81eef2971e2dc7e9345ba5a
Jiefeng Sun <jacksee@linuxg3.engr.colostate.edu>
2023-04-21 08:15:59 -0600

    move sh file to python folder

--- run_train.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-python3 train.py --model-dir=models/terrains_inddor --workers=10 --epochs=10 -b=32 data/terrains_indoor
-python3 onnx_export.py --model-dir=models/terrains_inddor
-python3 onnx_validate.py models/terrains_inddor/resnet18.onnx
